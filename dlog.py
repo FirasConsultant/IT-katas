dc32db52999b9a54b287289206263c08d026cadd
Luca De Feo <dfl@deagol>
2013-04-29 02:12:12 +0200

    Changed structure of dlog to simplify storytelling

--- dlog.py
+++ dlog.py
@@ -8,26 +8,15 @@ from google.appengine.ext import db
 
 
 levels = [
-    (20, 1048583, 5,
-     ['']),
-    (25, 33554467, 2,
-     ['']),
-    (30, 1073741827, 2, 
-     ['']),
-    (32, 4294967311, 3,
-     ['']),
-    (35, 34359738421, 2,
-     ['']),
-    (40, 1099511627791, 3,
-     ['']),
-    (50, 1125899906842679, 11,
-     ['']),
-    (60, 1152921504606847009, 13,
-     ['']),
-    (70, 1180591620717411303449, 3,
-     ['']),
-    (None, None, None,
-     ['']),
+    (20, 1048583, 5),
+    (25, 33554467, 2),
+    (30, 1073741827, 2),
+    (32, 4294967311, 3),
+    (35, 34359738421, 2),
+    (40, 1099511627791, 3),
+    (50, 1125899906842679, 11),
+    (60, 1152921504606847009, 13),
+    (70, 1180591620717411303449, 3),
 ]
 
 
@@ -57,7 +46,7 @@ def init(meth):
             self._level = self._max_level
         self._level = max(min(self._level, self._max_level), 0)
 
-        self._bits, self._p, self._g, self._text = levels[self._level]
+        self._bits, self._p, self._g = levels[self._level]
         random.seed(self._agent_at + str(self._level))
         self._exp = self._p and random.randrange(2 ** (self._bits//2 + 1), 2 ** self._bits)
 
@@ -70,54 +59,69 @@ class DLog(TH):
     @mission(2013, 5, 10, 4)
     @init
     def get(self):
-        self.cnr('dlog.html',
-                 agent=self._agent,
-                 countdown=self._countdown,
-                 static=self._static,
-                 solve=self._solve,
-                 level=self._level,
-                 max_level=self._max_level,
-                 text=self._text,
-                 p=self._p,
-                 gen=self._g,
-                 exp=self._exp,
-                 value=self._p and pow(self._g, self._exp, self._p),
-                 play_uri=self.uri_for(DLogPlay.__name__)
-                 )
+        mission = self.jinja2.render_template(
+            'dlog_levels.html',
+            static=self._static,
+            solve=self._solve,
+            agent=self._agent,
+            level=self._level,
+            p=self._p,
+            gen=self._g,
+            value=self._p and pow(self._g, self._exp, self._p),
+            ).split('<!-- level -->')[self._level].split('<!-- then -->')
 
-class DLogPlay(TH):
-    @mission(2013, 5, 10, 4)
-    @init
-    def get(self):
-        try:
-            log = int(self.request.get('log'))
-        except (ValueError, TypeError):
-            return self.cnr('dlog_fail.html',
-                            agent=self._agent,
-                            countdown=self._countdown,
-                            static=self._static,
-                            solve=self._solve,
-                            level=self._level,
-                            uri=self.uri_for(DLog.__name__))
+        message = mission[1] if len(mission) > 1 else None
+        mission = mission[0]
 
-        if (self._exp - log) % (self._p - 1):
-            return self.cnr('dlog_fail.html',
-                            agent=self._agent,
-                            countdown=self._countdown,
-                            static=self._static,
-                            solve=self._solve,
-                            level=self._level,
-                            uri=self.uri_for(DLog.__name__))
+        # This parameter means the user is playing
+        log = self.request.get('log')
+        if not log:
+            self.cnr('dlog.html',
+                     countdown=self._countdown,
+                     static=self._static,
+                     solve=self._solve,
+                     level=self._level,
+                     max_level=self._max_level,
+                     exp=self._exp,
+                     mission=mission,
+                     )
+        else:
+            try:
+                log = int(log)
+            except ValueError, TypeError:
+                advance = 0
+            else:
+                if (self._exp - log) % (self._p - 1):
+                    advance = 0
+                else:
+                    advance = 1
+                    if self._level == self._player.level:
+                        self._player.level += 1
+                        self._player.put()
+                        logging.warning('%s broke level %s'
+                                        % (self.session['user'].friendly(), 
+                                           self._level))
 
-        if self._level == self._player.level:
-            self._player.level += 1
-            self._player.put()
-            logging.warning('%s broke level %s'
-                            % (self.session['user'].friendly(), 
-                               self._level))
-            
-        return self.redirect_to(DLog.__name__, level=self._level + 1)
+            next_level = self._level + advance
+            next = self.uri_for(DLog.__name__,
+                            level=next_level,
+                            _full=True)
+            if self._static:
+                next += '&static'
+            if self._solve:
+                next += '&solve'
 
+            if not advance or message is None:
+                self.redirect(next)
+            else:
+                self.response.headers.add('Refresh', '5; url=%s' % next)
+                self.cnr('dlog_play.html',
+                         agent=self._agent,
+                         countdown=self._countdown,
+                         static=self._static,
+                         solve=self._solve,
+                         fail=not advance,
+                         message=message,
+                         uri=next)
 
-routes = [('', DLog),
-          ('play', DLogPlay)]
+routes = [('', DLog)]
