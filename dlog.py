d480521549949123c92955633635ceb13f59e748
Luca De Feo <dfl@deagol>
2013-05-02 07:08:20 +0200

    Finished storyline for Dlog

--- dlog.py
+++ dlog.py
@@ -5,7 +5,7 @@ from mission import mission
 import random
 import logging
 from google.appengine.ext import db
-
+from datetime import datetime
 
 levels = [
     (20, 1048583, 5),
@@ -18,7 +18,6 @@ levels = [
     (60, 1152921504606847009, 13),
     (70, 1180591620717411303449, 3),
 ]
-
 
 # Models
 class Dlog(db.Model):
@@ -48,7 +47,21 @@ def init(meth):
 
         self._bits, self._p, self._g = levels[self._level]
         random.seed(self._agent_at + str(self._level))
-        self._exp = self._p and random.randrange(2 ** (self._bits//2 + 1), 2 ** self._bits)
+        if self._level == 3:
+            check = 10
+            while check == 10:
+                isbn = random.randrange(10 ** 8, 10 ** 9)
+                check = reduce(lambda x,(y,z): (x-int(y)*z)%11, zip(str(isbn), range(10,1,-1)), 0)
+            self._exp = isbn*10 + check
+        elif self._level == 5:
+            self._exp = random.randrange(2 ** (self._bits//2 + 1), 10**12)
+        else:
+            self._exp = random.randrange(2 ** (self._bits//2 + 1), 2 ** self._bits)
+
+        if self._level == 1:
+            self._exp2 = random.randrange(2 ** (self._bits//2 + 1), 2 ** self._bits)
+        else:
+            self._exp2 = None
 
         return meth(self, *args, **kw)
 
@@ -61,16 +74,24 @@ class DLog(TH):
     def get(self):
         mission = self.jinja2.render_template(
             'dlog_levels.html',
+            countdown=self._countdown,
+            today=datetime.now(),
             static=self._static,
             solve=self._solve,
             agent=self._agent,
+            realname=self.session['user'].friendly(),
             level=self._level,
             p=self._p,
             gen=self._g,
-            value=self._p and pow(self._g, self._exp, self._p),
-            ).split('<!-- level -->')[self._level].split('<!-- then -->')
+            exp=self._exp,
+            value=pow(self._g, self._exp, self._p),
+            value2=self._exp2 and pow(self._g, self._exp2, self._p),
+            ).split('<!-- level -->')[self._level]
 
+        mission = mission.split('<!-- then -->')
         message = mission[1] if len(mission) > 1 else None
+        mission = mission[0].split('<!-- fail -->')
+        fail_msg = mission[1] if len(mission) > 1 else None
         mission = mission[0]
 
         # This parameter means the user is playing
@@ -91,37 +112,47 @@ class DLog(TH):
             except ValueError, TypeError:
                 advance = 0
             else:
-                if (self._exp - log) % (self._p - 1):
-                    advance = 0
-                else:
+                if self._level == 1:
+                    advance = int((pow(self._g, self._exp*self._exp2, self._p) - log) % self._p == 0)
+                elif (self._exp - log) % (self._p - 1) == 0:
                     advance = 1
-                    if self._level == self._player.level:
-                        self._player.level += 1
-                        self._player.put()
-                        logging.warning('%s broke level %s'
-                                        % (self.session['user'].friendly(), 
-                                           self._level))
+                else:
+                    advance = 0
+                    
+            if advance and self._level == self._player.level:
+                self._player.level += 1
+                self._player.put()
+                logging.warning('%s broke level %s'
+                                % (self.session['user'].friendly(), 
+                                   self._level))
+            elif not advance and self._level == len(levels) - 1:
+                self._player.level = 0
+                self._player.put()
+                logging.warning('%s game over'
+                                % self.session['user'].friendly())
 
             next_level = self._level + advance
-            next = self.uri_for(DLog.__name__,
-                            level=next_level,
-                            _full=True)
-            if self._static:
-                next += '&static'
-            if self._solve:
-                next += '&solve'
+            if next_level < len(levels):
+                next = self.uri_for(DLog.__name__,
+                                    level=next_level,
+                                    _full=True)
+                if self._static:
+                    next += '&static'
+                if self._solve:
+                    next += '&solve'
+            else:
+                next = None
 
-            if not advance or message is None:
+            if advance and message is None:
                 self.redirect(next)
             else:
-                self.response.headers.add('Refresh', '5; url=%s' % next)
                 self.cnr('dlog_play.html',
                          agent=self._agent,
                          countdown=self._countdown,
                          static=self._static,
                          solve=self._solve,
                          fail=not advance,
-                         message=message,
+                         message=(advance and message) or fail_msg,
                          uri=next)
 
 routes = [('', DLog)]
